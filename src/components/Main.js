a9fec5b94a59df4b8167b987af9bb7d262519507
yangqian <973860441@qq.com>
2016-11-14 23:24:58 +0800

    image

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,19 +10,42 @@ imageDatas = (function genImageURL(imageDatasArr){
 	for(let i=0;i<imageDatasArr.length;i++){
 		let singleImageData = imageDatasArr[i];
 
-		singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+		singleImageData.imageURL = require('../images/' + singleImageData.filename);
 		imageDatasArr[i] = singleImageData;
 	}
 	return imageDatasArr;
 })(imageDatas);
 
+class ImgFigure extends React.Component {
+  render(){
+    return (
+        <figure className="img-figure">
+          <img src={this.props.data.imageURL}
+                alt={this.props.data.title}/>
+          <figcaption>
+            <h2 className="img-title">{this.props.data.title}</h2>
+          </figcaption>
+        </figure>
+      );
+  }
+}
+
 class AppComponent extends React.Component {
   render() {
+    let controllerUnits = [],
+        imgFigures = [];
+
+    imageDatas.forEach(function(value){
+      imgFigures.push(<ImgFigure data={value}/>);
+    })
+
     return (
       <section className="stage">
         <section className="img-sec">
+          {imgFigures}
         </section>
         <nav className="controller-nav">
+          {controllerUnits}
         </nav>
       </section>
     );
